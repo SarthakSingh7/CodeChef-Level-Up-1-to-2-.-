e2fa7eda75ecfc4911246165ea537eb8a2a77307
Sarthak Singh <75946168+SarthakSingh7@users.noreply.github.com>
2022-07-29 03:51:09 +0530

    Create PROBCAT.py

---
+++ Basic Programming Concepts/PROBCAT.py
@@ -0,0 +1,9 @@
+for i in range(int(input())):
+    tc = int(input())
+    if 1 <= tc < 100:
+        print('Easy')
+    if 100 <= tc < 200:
+        print('Medium')
+    if 200 <= tc <= 300:
+        print('Hard')
+        
