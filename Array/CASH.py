4659d28fa833d3c8a35ebe278d2bf7af97e27096
Sarthak Singh <75946168+SarthakSingh7@users.noreply.github.com>
2022-09-07 00:03:13 +0530

    Update CASH.py

--- Array/CASH.py
+++ Array/CASH.py
@@ -3,5 +3,9 @@ for i in range(int(input())):
     arr = map(int,input().split())
     s_arr = sum(arr) # sum of the elements of the array
     print(s_arr%k)
+# the sum of the digits will also be divisble by k
+# the modulys operator on the sum
+# sum%k gives us the remainder 
+    
     
     
